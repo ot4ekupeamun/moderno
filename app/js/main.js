6f0600c634062b76b420dcb5ef8b86485b4955a0
ot4ekupeamun <prianig@mail.ru>
2021-04-25 23:35:26 +0300

    added adaptive product-details, new-items, index pages

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,30 @@ $(function () {
         slidesToScroll: 4,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
     });
 
 
@@ -19,7 +43,7 @@ $(function () {
     $('input[type="file"], select').styler();
 
 
-    
+
     $(".js-range-slider").ionRangeSlider({
         type: "double",
         min: 0,
@@ -31,15 +55,17 @@ $(function () {
 
 
     $('.icon-th-list').on('click', function () {
-        $('.product__item').addClass('list');
+        $('.product-page__items .products__item').addClass('list');
         $('.icon-th-list').addClass('active');
-        $('.icon-th-large').removeClass('active');
+        $('.icon-th-large ').removeClass('active');
     });
-    $('.icon-th-large').on('click', function () {
-        $('.product__item').removeClass('list');
-        $('.icon-th-large').addClass('active');
+
+    $('.icon-th-large ').on('click', function () {
+        $('.product-page__items .products__item').removeClass('list');
+        $('.icon-th-large ').addClass('active');
         $('.icon-th-list').removeClass('active');
     });
+
 
     $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
@@ -49,6 +75,8 @@ $(function () {
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
+
+
 
     $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
